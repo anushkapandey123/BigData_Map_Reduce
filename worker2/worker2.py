bae0a36f085d170a7a8f517abfe3ba728f8ce2b0
anushkapandey123 <93965660+anushkapandey123@users.noreply.github.com>
2023-02-24 23:22:49 +0530

    Add folder via upload

---
+++ worker2/worker2.py
@@ -0,0 +1,122 @@
+from flask import Flask, jsonify
+from flask import request
+from multiprocessing import Pool, TimeoutError
+import subprocess
+import time
+import os
+import multiprocessing
+
+
+app = Flask(__name__)
+	
+@app.route('/process', methods=["POST"])
+#def process():
+def process():
+	l = open("/home/pes1ug20cs072/BD_Project/log.txt", 'a')
+	content = request.json
+	print(content)
+	
+	f = open("names.txt", "w")
+	l.write("/home/pes1ug20cs072/BD_Project/worker2/names.txt\n")
+	for i in content:
+		j = str(i)
+		data = content[j]
+		
+		f.write(data)
+		f.write('\n')
+	
+	f.close()
+	
+	print("successfully created the file")
+	return jsonify(request.json)
+	
+	
+
+@app.route('/mapper', methods=["POST"])
+def mapper():
+	content = request.json
+	#print(type(content))
+	
+	f1 = open("mapper.py", "w")
+	print("successfully created the file")
+	i = 0
+	
+	for i in content:
+		if i == len(content):
+			break
+		j = str(i)
+		data = content[j]
+		
+		f1.write(data)
+	f1.close()
+	value = content[i]
+	print(value)
+	#print(value))
+	subprocess.call(['python3', 'mapper.py', value])
+	#p = subprocess.Popen(['python3', 'mapper.py', 'names1.txt'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)	
+	return jsonify(request.json)
+	
+@app.route('/reducer', methods=["POST"])
+def reducer():
+	content = request.json
+	f1 = open("reducer.py", "w")
+	print("successfully created the file")
+	i=0
+	
+	for i in content:
+		'''if i == len(content):
+			break'''
+		j = str(i)
+		data = content[j]
+		
+		f1.write(data)
+	f1.close()
+	#value = content[i]
+	#subprocess.call(['python3', '/home/pes1ug20cs072/BD_Project/shuffle.py'])
+	subprocess.call(['python3', 'reducer.py'])
+	return jsonify(request.json)
+	
+	
+
+if __name__ == '__main__':
+	app.run(port=3002, debug=True)	
+
+
+	
+
+
+	
+
+'''@app.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		f = request.files['the_file']
+		f.save('./abc.py')
+	return "<p>File received</p>"
+'''
+
+
+
+
+
+	
+
+
+	
+
+
+	
+
+'''@app.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		f = request.files['the_file']
+		f.save('./abc.py')
+	return "<p>File received</p>"
+'''
+
+
+
+
+
+	
